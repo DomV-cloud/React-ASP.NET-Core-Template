c166a9968490c7dca76cd91cc26cfb51ec13f373
Petr Barabáš <petr.barabas@memos.cz>
2023-08-06 13:55:31 +0200

    Created DbContext

---
+++ Car-E-shop/Database/EShopContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Car_E_shop.Database
+{
+    public class EShopContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
